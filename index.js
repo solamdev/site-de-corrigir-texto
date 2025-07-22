3bba0cef6821d8cb76d2a238d3c809ba9faf4c85
solamdev <lscheffer740@gmail.com>
2025-07-22 13:55:57 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const btt = document.querySelector('.btt')
+const input = document.querySelector('.input')
+const output = document.querySelector('.output')
+const form = document.querySelector('.form')
+const btnInformal = document.querySelector('.btn-informal')
+const btnEquilibrado = document.querySelector('.btn-equilibrado')
+const btnFormal = document.querySelector('.btn-formal')
+
+let active = "equilibrado"
+
+btnEquilibrado.addEventListener('click', async () => { 
+  btnFormal.classList.remove('active')
+  btnInformal.classList.remove('active')
+  btnEquilibrado.classList.add('active')
+  active = "equilibrado"
+  console.log(active)
+})
+
+btnFormal.addEventListener('click', async () => { 
+  btnFormal.classList.add('active')
+  btnInformal.classList.remove('active')
+  btnEquilibrado.classList.remove('acitive')
+  btnEquilibrado.classList.remove('active')
+  active = "formal"
+  console.log(active)
+  
+})
+
+btnInformal.addEventListener('click', async () => { 
+  btnFormal.classList.remove('active')
+  btnInformal.classList.add('active')
+  btnEquilibrado.classList.remove('acitive')
+  btnEquilibrado.classList.remove('active')
+  active = "informal"
+  console.log(active)
+})
+
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const valorInput = input.value 
+  console.log(valorInput)
+
+  const responce = await fetch("http://localhost:3000/chat" ,{
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({"texto": valorInput, "agr": active})
+  } 
+  )
+  try{
+     const data = await responce.json()
+     output.value = data.msg
+  }catch(error){
+    console.error(error)
+  }
+ 
+})
+
